d1b8c7f76831acd6a306613a0a7035c5d6a7f4ba
Shannon Rivers <shannon.rivers@gmail.com>
2017-04-19 09:32:18 -0600

    should be functioning getSum

--- w03/w3-r-sum-of-numbers/problem.js
+++ w03/w3-r-sum-of-numbers/problem.js
@@ -1,10 +1,10 @@
-/* Given two integers, which can be positive and negative, 
+/* Given two integers, which can be positive and negative,
 find the sum of all the numbers between and including a and b,
 and return the sum. If both numbers are equal return a or b.
 
 Note! a and b are not ordered!
 
-Example: 
+Example:
 getSum(1, 0) == 1   // 1 + 0 = 1
 getSum(1, 2) == 3   // 1 + 2 = 3
 getSum(0, 1) == 1   // 0 + 1 = 1
@@ -14,7 +14,19 @@ getSum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
 */
 
 function getSum( a, b ) {
-
+  var sumThis = [];
+  if (a===b){
+    return a;
+  } else if (b>a){
+    for (let i=a; i<=b; i++){
+      sumThis.push(i);
+    }
+  } else if (a>b){
+    for (let i=b; i<=a; i++){
+      sumThis.push(i);
+    }
+  }
+  return sumThis.reduce( (prev, curr) => prev + curr );
 } // END FUNCTION
 
 
@@ -22,5 +34,3 @@ module.exports = {
   getSum:getSum,
   attendance:"WORD UP"
 }
-
-
