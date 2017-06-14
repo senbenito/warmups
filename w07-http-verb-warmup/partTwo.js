2247ad1221e636992d29608685ebe940c6e3c7b6
Shannon Rivers <shannon.rivers@gmail.com>
2017-06-14 10:12:38 -0600

    working through w07- part2

--- w07-http-verb-warmup/partTwo.js
+++ w07-http-verb-warmup/partTwo.js
@@ -1,10 +1,49 @@
 // 🐾🐾🐾 PART TWO 🐾🐾🐾
 
 var parseRequest = function (inputString) {
-  return;
-} // END FUNCTION
+  var result = {
+    body:{
+
+    },
+    headers:
+   { Host: '',
+     'Cache-Control': '',
+     'Postman-Token': '' },
+  verb: '',
+  path: '',
+  protocol: '' };
+
+  let splitStringSpace = inputString.split(' ', 3);
+  let splitStringLine = inputString.split('\n');
+
+//verb
+  result.verb = splitStringSpace[0];
+
+//path
+  result.path = splitStringSpace[1];
+
+//protocol
+  result.protocol = splitStringSpace[2].split('\n')[0];
+
+//body object
+  for (let i=0; i<splitStringLine.length-4; i++){
+    let splitStringAnd = splitStringLine[splitStringLine.length-1].split('&');
+    // console.log(splitStringAnd);
+    let splitStringEqual = splitStringAnd[i].split('=');
+    console.log(splitStringEqual);
+    result.body[splitStringEqual[0]]=splitStringEqual[1];
+}
+
+//headers object
+  for (let i=1; i<splitStringLine.length-1; i++){
+    let splitStringColon = splitStringLine[i].split(':');
+    result.headers[splitStringColon[0]]=splitStringColon[1];
+}
+
+  return result;
+}; // END FUNCTION
 
 module.exports = {
   parseRequest: parseRequest,
   attendance: "wordy wordy"
-}
+};
